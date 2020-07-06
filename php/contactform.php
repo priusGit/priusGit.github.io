d294c906cc2c09c0a392e1656a9684b3ed28c20a
priusGit <mrszaro2000@wp.pl>
2020-07-06 16:18:40 +0200

    .

--- php/contactform.php
+++ php/contactform.php
@@ -10,5 +10,5 @@ if(isset($_POST['submit'])){
     $txt = "You have recieved an email from ".$name.".\n\n".$message;
 
     mail($mailTo,$subject,$txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html");
 }
